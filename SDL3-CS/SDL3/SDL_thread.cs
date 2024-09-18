65e77722e08a76b80e5780d802171b94877f3b47
Dan Balasescu <smoogipoo@smgi.me>
2024-09-19 02:47:17 +0900

    Fix some more binding generation on unix platforms

--- SDL3-CS/SDL3/SDL_thread.cs
+++ SDL3-CS/SDL3/SDL_thread.cs
@@ -10,4 +10,13 @@ namespace SDL
 
     [Typedef]
     public enum SDL_ThreadID : UInt64;
+
+    public static partial class SDL3
+    {
+        [Constant]
+        public const int SDL_BeginThreadFunction = 0;
+
+        [Constant]
+        public const int SDL_EndThreadFunction = 0;
+    }
 }
