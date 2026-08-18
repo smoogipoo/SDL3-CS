cec04fde47d1bd9c71a3fbd8173a8b5e86952f14
Dan Balasescu <smoogipoo@smgi.me>
2026-08-18 18:50:17 +0900

    NET8.0

--- SDL3-CS.Tests.iOS/Main.cs
+++ SDL3-CS.Tests.iOS/Main.cs
@@ -1,23 +1,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using SDL;
+using static SDL.SDL3;
 
 public class Program
 {
     public static unsafe void Main(string[] args)
     {
         NativeLibrary.SetDllImportResolver(typeof(SDL3).Assembly, (_, assembly, path) => NativeLibrary.Load("@rpath/SDL3.framework/SDL3", assembly, path));
-        NativeLibrary.SetDllImportResolver(typeof(SDL3_image).Assembly, (_, assembly, path) => NativeLibrary.Load("@rpath/SDL3_image.framework/SDL3_image", assembly, path));
-        NativeLibrary.SetDllImportResolver(typeof(SDL3_ttf).Assembly, (_, assembly, path) => NativeLibrary.Load("@rpath/SDL3_ttf.framework/SDL3_ttf", assembly, path));
-        NativeLibrary.SetDllImportResolver(typeof(SDL3_mixer).Assembly, (_, assembly, path) => NativeLibrary.Load("@rpath/SDL3_mixer.framework/SDL3_mixer", assembly, path));
-
-        SDL3.SDL_RunApp(0, null, &main, IntPtr.Zero);
+        SDL_RunApp(0, null, &main, IntPtr.Zero);
     }
 
     [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
     private static unsafe int main(int argc, byte** argv)
     {
-        SDL.Tests.Program.Main();
+        // Required to enable calls to GetDisplays() and SetGamepadEventsEnabled().
+        SDL_InitSubSystem(SDL_InitFlags.SDL_INIT_VIDEO | SDL_InitFlags.SDL_INIT_GAMEPAD);
+
+        // I don't know why this is relevant, but:
+        // 1. It must be an interpolation (e.g. "abc" doesn't work).
+        // 2. It must have a decent size (e.g. $"X: {argc}" is too short).
+        Console.WriteLine($"Dummy value: {argc}");
+
+        // I don't think it's particularly important what SDL does internally here.
+        int count;
+        Console.WriteLine((IntPtr)SDL_GetDisplays(&count));
+
+        // Segfault occurs on the following pinvoke.
+        SDL_SetGamepadEventsEnabled(true);
+
         return 0;
     }
 }
